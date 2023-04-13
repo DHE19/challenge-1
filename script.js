39454e0fc8bc9ac8b1843e69c3857f93988d12e5
dhe19 <info.die@gmail.com>
2023-04-13 03:58:23 -0600

    encrypt, decrypt and copy

--- script.js
+++ script.js
@@ -1,20 +1,78 @@
-let buttonEcript = document.getElementById('btn-encript');
-let buttonDecript = document.getElementById('btn-dencript');
-let textAreaInput = document.getElementById('message');
-let textAreaOutput = document.getElementById('decrypt-area');
+//INICIALIZAMOS LAS VARIABLES NECESARIAS
+let encryptButton = document.getElementById('btn-encript');
+let decryptButton = document.getElementById('btn-dencript');
+let inputTextArea = document.getElementById('message');
+let outputTextArea = document.getElementById('decrypt-area');
 let Infomenu = document.getElementById('placeholder');
-let panelOutput = document.getElementById('panel-message');
+let outputPanel = document.getElementById('panel-message');
+let copyButton = document.getElementById('copy');
 
-
-
-
-buttonDecript.onclick = () => {
-    console.log('funcionando perfectamente');
-}
-buttonEcript.onclick = () => {
-    let message = textAreaInput.value;
-    panelOutput.style.display="flex"
-    panelOutput.style.animation = "showlol 1.5s";
+decryptButton.onclick = () => {
+    let message = inputTextArea.value;
+    outputPanel.style.display = "flex"
+    outputPanel.style.animation = "showlol 1.5s";
     Infomenu.style.display = 'none';
-    textAreaOutput.value = message;
+    outputTextArea.value = decrypt(message);
+}
+encryptButton.onclick = () => {
+    let message = inputTextArea.value;
+    outputPanel.style.display = "flex"
+    outputPanel.style.animation = "showlol 1.5s";
+    Infomenu.style.display = 'none';
+    outputTextArea.value = encrypt(message);
+}
+
+copyButton.onclick = () =>{
+    outputTextArea.select();
+    document.execCommand("copy");
+}
+//encripta los mensajes
+const encrypt = (message) => {
+
+    let vocales = {
+        a: 'ai',
+        e: 'enter',
+        i: 'imes',
+        o: 'ober',
+        u: 'ufat'
+    }
+    
+    return iteratorMessage(message, 
+        (e) =>{
+        for (const vocal in vocales) 
+            if (e === vocal) return vocales[vocal];
+        return e;
+        }, '')
+}
+//des encripta los mensajes
+const decrypt = (message) => {
+
+    let specialWords = {
+        ai: 'a',
+        enter:'e',
+        imes: 'i',
+        ober: 'o',
+        ufat: 'u',
+    }
+
+    return iteratorMessage(message,(e) =>{
+        let current = e;
+        for (const word in specialWords) {
+            let regex = new RegExp(word, 'gi');
+            current = current.replace(regex, specialWords[word]);
+        }
+        return current;
+    }, ' ');
+}
+
+
+
+// particiona el mensaje acorde a la tercera variable, y usamos action para definir la forma en que se va tratar
+//los datos
+const iteratorMessage = (message, action,joinNSplitWords) =>{
+    return message
+        .split(joinNSplitWords)
+        .map(e => action(e))
+        .join(joinNSplitWords)
+
 }
